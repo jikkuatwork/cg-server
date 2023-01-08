2548afd64977650132688234f9d73372562160c4
Jikku Jose <jikku@storybrain.com>
2023-01-09 00:24:18 +0530

    Update ntfy handler

--- ntfy_sub.js
+++ ntfy_sub.js
@@ -1,17 +1,34 @@
-const EventSource = require("eventsource")
 const fs = require("fs")
+const EventSource = require("eventsource")
+const dotenv = require("dotenv")
 
-const data = "This is the data that you want to write to the file"
+dotenv.config()
 
-fs.writeFile("/path/to/file", data, error => {
-  if (error) {
-    console.error(error)
-    return
+const SWAP_FILE = process.env.SWAP_FILE
+const NTFY_ASK_TOKEN = process.env.NTFY_ASK_TOKEN
+
+const formatToQuestion = query => {
+  const request = JSON.parse(JSON.parse(query).message)
+
+  switch (request.type) {
+    case "explain":
+      return `What are the main points of ${request.content}, using as minimal words as possible and avoiding repetition?`
+    case "rewrite":
+      return `Rewrite the following for better readability: ${request.content}`
+    default:
+      return "The default question!"
   }
-  console.log("The file has been saved!")
-})
+}
 
-const eventSource = new EventSource("https://ntfy.sh/jj-cg-ask/sse")
+const eventSource = new EventSource(`https://ntfy.sh/${NTFY_ASK_TOKEN}/sse`)
 eventSource.onmessage = e => {
-  console.log(e.data)
+  const question = formatToQuestion(e.data)
+  console.log(question)
+
+  fs.writeFile(SWAP_FILE, question, error => {
+    if (error) {
+      console.error(error)
+      return
+    }
+  })
 }
