13db224483951e19aaff7a1f7b73eeada3326fde
Jikku Jose <jikku@storybrain.com>
2023-01-11 01:36:27 +0530

    Refactor

--- ntfy_sub.js
+++ ntfy_sub.js
@@ -7,18 +7,7 @@ dotenv.config()
 const SWAP_FILE = process.env.SWAP_FILE
 const NTFY_ASK_TOKEN = process.env.NTFY_ASK_TOKEN
 
-const formatToQuestion = query => {
-  const request = JSON.parse(JSON.parse(query).message)
-
-  switch (request.type) {
-    case "explain":
-      return `What are the main points of ${request.content}, using as minimal words as possible and avoiding repetition?`
-    case "rewrite":
-      return `Rewrite the following for better readability: ${request.content}`
-    default:
-      return "The default question!"
-  }
-}
+const formatToQuestion = query => JSON.parse(query).message
 
 const eventSource = new EventSource(`https://ntfy.sh/${NTFY_ASK_TOKEN}/sse`)
 eventSource.onmessage = e => {
